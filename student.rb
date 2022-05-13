7d45d4330e1a4c8db0393698b8f1c55174f5898c
Kenny <kennysalazar3197@gmail.com>
2022-05-12 22:37:29 -0500

    Add belongs-to classroom relationship to Student class

--- student.rb
+++ student.rb
@@ -1,9 +1,12 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
