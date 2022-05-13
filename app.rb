b7e6e2c878ec7e5db7e6b3c783784bfa112258e3
Kenny <kennysalazar3197@gmail.com>
2022-05-13 15:19:39 -0500

    Create mthod for adding a student

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require './book'
+require './classroom'
+require './student'
+require './teacher'
 
 class App
   def initialize
+    @classroom = Classroom.new('Microverse classroom')
     @books_list = []
+    @people_list = []
   end
 
-  def list_all_books
+  def list_books
     @books_list
   end
 
@@ -13,4 +18,11 @@ class App
     @books_list << Book.new(title, author)
   end
 
+  def list_people
+    @people_list
+  end
+
+  def create_student(age, name = 'unknown', has_permission: true)
+    @people_list << Student.new(@classroom, age, name, parent_permission: has_permission)
+  end
 end
