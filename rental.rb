b6a0a79172220ef46f4736141068076bf7450630
Kenny <kennysalazar3197@gmail.com>
2022-05-12 22:50:33 -0500

    Add belongs_to relationhsips to Rental class

--- rental.rb
+++ rental.rb
@@ -1,7 +1,12 @@
 class Rental
   attr_accessor :date
+  attr_reader :book, :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+    @book = book
+    book.add_rental(self)
+    @person = person
+    person.add_rental(self)
   end
 end
