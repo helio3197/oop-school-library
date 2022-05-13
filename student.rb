e581c2a17c4460a06984bb785fb61368d912b409
Kenny Salazar <kennysalazar3197@gmail.com>
2022-05-12 23:26:47 -0500

    Update student.rb
    Co-authored-by: Mugirase Emmanuel <emmanuellamugi@gmail.com>

--- student.rb
+++ student.rb
@@ -6,7 +6,7 @@ class Student < Person
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom.add_student(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
