e03f7e3ccef6288785f1a130064b1d178eb7c7a9
Kenny <kennysalazar3197@gmail.com>
2022-05-18 15:41:43 -0500

    Update tests for Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,8 +2,21 @@ require 'rspec'
 require './book'
 
 describe Book do
-  it 'some test' do
-    book = Book.new('Test Title', 'Test Author')
-    expec(book.id).to instance_of Number
+  context 'Tests for the Book class' do
+    before(:all) do
+      @book = Book.new('Test Title', 'Test Author')
+    end
+
+    it 'A book object should contain an id attribute' do
+      expect(@book.id).to be_instance_of Integer
+    end
+
+    it 'A book object should contain a title attribute' do
+      expect(@book.title).to eql 'Test Title'
+    end 
+
+    it 'Abook object should contain an author attribute' do
+      expect(@book.author).to eql 'Test Author'
+    end
   end
 end
